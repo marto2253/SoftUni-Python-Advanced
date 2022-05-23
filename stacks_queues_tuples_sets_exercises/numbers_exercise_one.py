dfa060bda835e205364bc4ce44bef85a8a124cc9
marto2253 <georgiev.martin1@gmail.com>
2022-05-23 20:41:45 +0300

    Adding exercises.

---
+++ stacks_queues_tuples_sets_exercises/numbers_exercise_one.py
@@ -0,0 +1,44 @@
+
+def numbers(seq_one, seq_two):
+
+    iterations = int(input())
+
+    for _ in range(iterations):
+
+        command = input().split()
+
+        if command[0] == 'Add':
+            if command[1] == 'First':
+                for i in command:
+                    if i.isdigit():
+                        seq_one.add(int(i))
+            else:
+                for i in command:
+                    if i.isdigit():
+                        seq_two.add(int(i))
+        elif command[0] == 'Remove':
+            if command[1] == 'First':
+                for i in command:
+                    if i.isdigit() and int(i) in seq_one:
+                        seq_one.remove(int(i))
+            else:
+                for i in command:
+                    if i.isdigit() and int(i) in seq_two:
+                        seq_two.remove(int(i))
+        else:
+            if seq_one.issubset(seq_two):
+                print('True')
+            elif seq_two.issubset(seq_one):
+                print('True')
+            else:
+                print('False')
+
+    print(", ".join(str(i) for i in sorted(seq_one)))
+    print(", ".join(str(i) for i in sorted(seq_two)))
+
+
+sequence_one = set([int(i) for i in input().split(' ')])
+sequence_two = set([int(i) for i in input().split(' ')])
+
+numbers(sequence_one, sequence_two)
+
